a9591b62fcb08ce76e2fa3180680113040d5c15f
Hoang218 <hoanghd218@gmail.com>
2024-05-01 15:34:13 +0700

    update

---
+++ RevitAddinFromNice3PointTemplate/RevitAddIn1/Bai4Parameter/ColumnBeamSlabConcreteVolume/ColumnBeamSlabConcreteVolumeCmd.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using Autodesk.Revit.Attributes;
+using Nice3point.Revit.Toolkit.External;
+
+namespace RevitAddIn1.Bai4Parameter.ColumnBeamSlabConcreteVolume
+{
+    [UsedImplicitly]
+    [Transaction(TransactionMode.Manual)]
+    public class ColumnBeamSlabConcreteVolumeCmd : ExternalCommand
+    {
+        public override void Execute()
+        {
+
+
+            var columnBeamSlabsFilter = new ElementMulticategoryFilter(new List<BuiltInCategory>()
+            {
+                BuiltInCategory.OST_StructuralColumns,
+                BuiltInCategory.OST_Floors,
+                BuiltInCategory.OST_StructuralFraming,
+            });
+
+            var columnBeamSlabs = new FilteredElementCollector(Document, ActiveView.Id)
+                .WherePasses(columnBeamSlabsFilter)
+                .WhereElementIsNotElementType()
+                .ToElements();
+
+            MessageBox.Show(columnBeamSlabs.Count.ToString());
+
+            var totalVolume = columnBeamSlabs.Sum(x => x.get_Parameter(BuiltInParameter.HOST_VOLUME_COMPUTED).AsDouble() * 0.02831685);
+            MessageBox.Show($"Total volume is {totalVolume} m3" );
+        }
+    }
+}
---
+++ RevitAddinFromNice3PointTemplate/RevitAddIn1/Bai4Parameter/SetMark/ColumnBeamSlabConcreteVolumeCmd.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.UI.Selection;
+using Nice3point.Revit.Toolkit.External;
+
+namespace RevitAddIn1.Bai4Parameter.SetMark
+{
+    [UsedImplicitly]
+    [Transaction(TransactionMode.Manual)]
+    public class SetMarkCmd : ExternalCommand
+    {
+        public override void Execute()
+        {
+            var columnRfs = UiDocument.Selection.PickObjects(ObjectType.Element, "Select columns");
+
+            var columns = columnRfs.Select(x => Document.GetElement(x)).ToList();
+
+
+
+            var markC1 = "C1";
+
+            using (var tx = new Transaction(Document, "Set mark"))
+            {
+                tx.Start();
+
+                columns.ForEach(x =>
+                {
+                    x.LookupParameter("Mark").Set(markC1);
+                    x.get_Parameter(BuiltInParameter.ALL_MODEL_INSTANCE_COMMENTS).Set("Comment for c1");
+                    x.get_Parameter(BuiltInParameter.ALL_MODEL_INSTANCE_COMMENTS).Set("Comment for c1");
+                    x.get_Parameter(BuiltInParameter.FAMILY_BASE_LEVEL_OFFSET_PARAM).Set(0.2);
+                });
+
+                tx.Commit();
+            }
+        }
+    }
+}
